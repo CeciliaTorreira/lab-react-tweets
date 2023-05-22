28c9ba0edeae8e310fd213e0890b55adc68a670d
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-22 15:27:35 +0100

    Empezada tercera iteración

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,11 @@
+function ProfileImage(props){
+    return(
+        <img
+        src="https://i.imgur.com/9yw1Fyw.jpg"
+        className="profile"
+        alt="profile"
+      />
+    )
+}
+
+export default ProfileImage
